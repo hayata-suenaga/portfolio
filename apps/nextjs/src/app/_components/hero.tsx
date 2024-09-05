57d59b8c9b65ad5f2ab283a502cb683b21dc68c8
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2024-09-05 23:59:21 +0900

    chore: hero

--- apps/nextjs/src/app/_components/hero.tsx
+++ apps/nextjs/src/app/_components/hero.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@acme/ui/button";
+import { ShootingStars } from "@acme/ui/shooting-stars";
+import { StarsBackground } from "@acme/ui/star-background";
 
 export default function Hero() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-slate-900 px-4 text-slate-50 sm:px-6 lg:px-8">
+    <div className="flex h-screen items-center px-6">
       <div className="mx-auto max-w-3xl text-center">
-        <h1 className="mb-4 bg-gradient-to-b from-gray-100 to-gray-600 bg-clip-text text-4xl font-extrabold tracking-tight text-transparent sm:text-5xl md:text-6xl">
+        <h1 className="mb-4 bg-gradient-to-b from-slate-500 to-white bg-clip-text text-4xl font-extrabold tracking-tight text-transparent sm:text-5xl md:text-6xl">
           Launch Your MVP
           <br />
           in 1 <span className="line-through decoration-slate-50">
@@ -13,12 +15,14 @@ export default function Hero() {
           week
         </h1>
         <p className="mb-8 text-xl text-gray-400 sm:text-2xl md:text-3xl">
-          with Hayata Suenaga
+          with{" "}
+          <span className="bg-gradient-to-r from-blue-500 via-purple-500 to-blue-500 bg-clip-text text-transparent">
+            Hayata Suenaga
+          </span>
         </p>
-        <Button className="rounded-md bg-white px-8 py-3 text-lg font-semibold text-black transition-colors duration-200 hover:bg-gray-200">
-          Get Started
-        </Button>
       </div>
+      <ShootingStars />
+      <StarsBackground />
     </div>
   );
 }
