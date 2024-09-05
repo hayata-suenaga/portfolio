3358b3d0036ca7fe73857098cdb69400b0766378
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2024-09-05 23:56:07 +0900

    chore: add components

--- packages/ui/src/timeline.tsx
+++ packages/ui/src/timeline.tsx
@@ -29,17 +29,13 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
   const opacityTransform = useTransform(scrollYProgress, [0, 0.1], [0, 1]);
 
   return (
-    <div
-      className="w-full bg-white font-sans dark:bg-neutral-950 md:px-10"
-      ref={containerRef}
-    >
+    <div className="w-full font-sans md:px-10" ref={containerRef}>
       <div className="mx-auto max-w-7xl px-4 py-20 md:px-8 lg:px-10">
         <h2 className="mb-4 max-w-4xl text-lg text-black dark:text-white md:text-4xl">
-          Changelog from my journey
+          My Changelog
         </h2>
-        <p className="max-w-sm text-sm text-neutral-700 dark:text-neutral-300 md:text-base">
-          I&apos;ve been working on Aceternity for the past 2 years. Here&apos;s
-          a timeline of my journey.
+        <p className="max-w-sm text-sm text-neutral-700 dark:text-slate-50/75 md:text-base">
+          I&apos;ve been coding for 5 years. Here&apos;s a concise changelog.
         </p>
       </div>
 
