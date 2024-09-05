5d1aabc2a0ffab4e677bf0ab760511b5b78fcd0f
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2024-09-05 21:33:26 +0900

    chore: configure tailwind

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 
 import baseConfig from "@acme/tailwind-config/web";
 
@@ -16,4 +17,16 @@ export default {
       },
     },
   },
+  plugins: [addVariablesForColors],
 } satisfies Config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
