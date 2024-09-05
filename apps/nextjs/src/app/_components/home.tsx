6052dd7174fc45988807bf1bc83c75c59cbca4d2
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2024-09-05 23:59:35 +0900

    chore: temporarily remove work history

--- apps/nextjs/src/app/_components/home.tsx
+++ apps/nextjs/src/app/_components/home.tsx
@@ -1,13 +1,11 @@
 import { Contact } from "./contact";
 import Hero from "./hero";
-import { SubHero } from "./sub-hero";
 import { WorkHistory } from "./work-history";
 
 export default function Home() {
   return (
     <div>
       <Hero />
-      <SubHero />
       <WorkHistory />
       <Contact />
     </div>
