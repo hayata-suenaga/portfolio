8c1fca7af3c776384bace715d4eec6bb1ae78056
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2024-09-06 01:16:29 +0900

    chore: add image

--- apps/nextjs/src/app/_components/hero.tsx
+++ apps/nextjs/src/app/_components/hero.tsx
@@ -1,10 +1,14 @@
+import Image from "next/image";
+
 import { BackgroundBeamsWithCollision } from "@acme/ui/background-beams-with-collision";
 import { StarsBackground } from "@acme/ui/star-background";
 
+import me from "../../../public/me.png";
+
 export default function Hero() {
   return (
-    <BackgroundBeamsWithCollision>
-      <div className="relative flex h-screen items-center px-6">
+    <div className="relative flex h-screen items-center px-6">
+      <BackgroundBeamsWithCollision>
         <div className="mx-auto max-w-3xl text-center">
           <h1 className="mb-4 bg-gradient-to-b from-slate-500 to-white bg-clip-text text-4xl font-extrabold tracking-tight text-transparent sm:text-5xl md:text-6xl">
             Launch Your MVP
@@ -21,8 +25,10 @@ export default function Hero() {
             </span>
           </p>
         </div>
-        <StarsBackground />
-      </div>
-    </BackgroundBeamsWithCollision>
+        <Image src={me} alt="Hayata" className="absolute bottom-0 h-80 w-80" />
+      </BackgroundBeamsWithCollision>
+
+      <StarsBackground />
+    </div>
   );
 }
