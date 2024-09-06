9d00f814e2f903deb61677a2e8e3355aea8c3ff5
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2024-09-06 11:00:08 +0900

    chore: organize code

--- apps/nextjs/src/app/_components/work-history.tsx
+++ apps/nextjs/src/app/_components/work-history.tsx
@@ -5,94 +5,78 @@ import { ShootingStars } from "@acme/ui/shooting-stars";
 import { StarsBackground } from "@acme/ui/star-background";
 import { Timeline } from "@acme/ui/timeline";
 
-const items_1 = [
-  "Managed teams of 3-6 engineers",
-  "Onboarded new engineers to Expensify's payment infrastructure",
-  "Co-led a TypeScript migration of Expensify's codebase",
-  "Authored TypeScript guidelines now used by the entire engineering team",
-];
-
-const items_2 = [
-  "Reported directly to CEO",
-  "Shipped more than 10 new features to production",
-];
-
-const items_3 = [
-  "Graduated with a 3.9 GPA",
-  "Awarded the Computer Science Department's highest honor",
-];
-
 export function WorkHistory() {
-  const data = [
-    {
-      title: "Expensify",
-      content: (
-        <div>
-          <p className="mb-8s mb-2 text-xl font-normal text-neutral-800 dark:text-neutral-200">
-            Senior Full Stack Engineer
-          </p>
-          <p className="mb-4 text-sm dark:text-slate-50/75">
-            Feb 2023 - August 2024
-          </p>
-          <ul className="space-y-2">
-            {items_1.map((item) => (
-              <li className="flex items-center gap-4">
-                <Minus className="text-blue-500/75" />
-                {item}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ),
-    },
-    {
-      title: "Dendron",
-      content: (
-        <div>
-          <p className="mb-8s mb-2 text-xl font-normal text-neutral-800 dark:text-neutral-200">
-            Summer Startup Resident
-          </p>
-          <p className="mb-4 text-sm dark:text-slate-50/75">
-            June 2022 - September 2022
-          </p>
-          <ul className="space-y-2">
-            {items_2.map((item) => (
-              <li className="flex items-center gap-4">
-                <Minus className="text-blue-500/75" />
-                {item}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ),
-    },
-    {
-      title: "Centre College",
-      content: (
-        <div>
-          <p className="mb-8s mb-2 text-xl font-normal text-neutral-800 dark:text-neutral-200">
-            BS in Computer Science
-          </p>
-          <p className="mb-4 text-sm dark:text-slate-50/75">
-            May 2019 - May 2022
-          </p>
-          <ul className="space-y-2">
-            {items_3.map((item) => (
-              <li className="flex items-center gap-4">
-                <Minus className="text-blue-500/75" />
-                {item}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ),
-    },
-  ];
   return (
     <div className="relative w-full">
-      <Timeline data={data} />
+      <Timeline data={DATA} />
       <ShootingStars />
       <StarsBackground />
+    </div>
+  );
+}
+
+const ITEMS = [
+  {
+    place: "Expensify",
+    title: "Senior Full Stack Engineer",
+    period: "Feb 2023 - Aug 2024",
+    items: [
+      "Managed teams of 3-6 engineers",
+      "Onboarded new engineers to Expensify's payment infrastructure",
+      "Co-led a TypeScript migration of Expensify's codebase",
+      "Authored TypeScript guidelines now used by the entire engineering team",
+    ],
+  },
+  {
+    place: "Dendron",
+    title: "Summer Startup Resident",
+    period: "Jun 2022 - Sep 2022",
+    items: [
+      "Reported directly to CEO",
+      "Shipped more than 10 new features to production",
+    ],
+  },
+  {
+    place: "Centre College",
+    title: "BS in Computer Science",
+    period: "Aug 2019 - May 2022",
+    items: ["On the Dean's List for four consecutive terms at Centre College"],
+  },
+];
+
+const DATA = ITEMS.map((item) => ({
+  title: item.place,
+  content: (
+    <Content title={item.title} subTitle={item.period} list={item.items} />
+  ),
+}));
+
+function Content({
+  title,
+  subTitle,
+  list,
+}: {
+  title: string;
+  subTitle: string;
+  list: string[];
+}) {
+  return (
+    <div>
+      <p className="mb-8s mb-2 text-xl font-normal text-neutral-800 dark:text-neutral-200">
+        {title}
+      </p>
+      <p className="mb-4 text-sm dark:text-slate-50/75">{subTitle}</p>
+      <ul className="space-y-2">
+        {list.map((item, index) => (
+          <li key={index} className="flex items-center gap-4">
+            <div>
+              <Minus className="text-blue-500/75" size={18} />
+            </div>
+
+            <span className="text-sm md:text-base">{item}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
