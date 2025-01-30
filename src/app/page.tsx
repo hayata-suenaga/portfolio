209ca63ec3dae9662db61cd1a17d8ee6efa22ba8
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2025-01-30 16:46:07 +0900

    feat: add a button to toggle between the dark and light theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Sun, Moon } from "lucide-react";
+import { useTheme } from "next-themes";
+
 export default function Home() {
+  const { setTheme, theme } = useTheme();
+
   return (
     <div className="flex justify-center items-center h-screen">
       Hello, World
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      >
+        <Sun className="h-[1.5rem] w-[1.3rem] dark:hidden" />
+        <Moon className="hidden h-5 w-5 dark:block" />
+        <span className="sr-only">Toggle theme</span>
+      </Button>
     </div>
   );
 }
