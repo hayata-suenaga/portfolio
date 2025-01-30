ce498afdf475a340c7ee642d4aff8d650624699b
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2025-01-30 16:05:38 +0900

    chore: center the text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Hello, World</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      Hello, World
+    </div>
+  );
 }
