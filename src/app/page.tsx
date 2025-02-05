c0afd39da446a940d4d31a99929089b9e67d9158
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2025-02-05 14:15:18 +0900

    chore: add info cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { GitHubCharts } from "@/components/github-charts";
 import HeroSection from "@/components/hero";
 import ExperienceSection from "@/components/experience";
 import { RoughNotation } from "react-rough-notation";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function PortfolioPage() {
   return (
@@ -21,31 +22,52 @@ export default function PortfolioPage() {
             >
               <h2 className="inline text-3xl font-bold">Who am I?</h2>
             </RoughNotation>
-            <article className="prose dark:prose-invert mt-12">
-              <p>
-                Hi! I&apos;m Hayata, a software engineer with over three years
-                of industry experience, much of which I spent at{" "}
-                <strong>Expensify</strong> working on systems serving{" "}
-                <u>15M+ users</u>. Based in San Francisco and sometimes in
-                Japan.
-              </p>
 
-              <p>
-                At Expensify, I led critical projects and managed engineering
-                teams. Now, I&apos;m building
-                <strong> Eisuke</strong>, a language learning platform that
-                combines web and mobile applications to help people master
-                English vocabulary.
-              </p>
+            <div className="grid md:grid-cols-2 gap-4 mt-12">
+              <div className="flex flex-col justify-center gap-8">
+                <Card className="max-w-72">
+                  <CardHeader>
+                    <CardTitle className="text-base">
+                      Years of Experience
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent>+3 years</CardContent>
+                </Card>
 
-              <p>
-                I focus on identifying common patterns that can be solved
-                through existing solutions, letting teams ship faster by
-                focusing purely on business logic. My recent interest is in{" "}
-                <u>natural language processing</u> and its applications in the
-                EdTech space.
-              </p>
-            </article>
+                <Card className="max-w-72">
+                  <CardHeader>
+                    <CardTitle className="text-base">Location</CardTitle>
+                  </CardHeader>
+                  <CardContent>San Francisco, CA / Tokyo, JP</CardContent>
+                </Card>
+              </div>
+
+              <article className="prose dark:prose-invert">
+                <p>
+                  Hi! I&apos;m Hayata, a software engineer with over three years
+                  of industry experience, much of which I spent at{" "}
+                  <strong>Expensify</strong> working on systems serving{" "}
+                  <u>15M+ users</u>. Based in San Francisco and sometimes in
+                  Japan.
+                </p>
+
+                <p>
+                  At Expensify, I led critical projects and managed engineering
+                  teams. Now, I&apos;m building
+                  <strong> Eisuke</strong>, a language learning platform that
+                  combines web and mobile applications to help people master
+                  English vocabulary.
+                </p>
+
+                <p>
+                  I focus on identifying common patterns that can be solved
+                  through existing solutions, letting teams ship faster by
+                  focusing purely on business logic. My recent interest is in{" "}
+                  <u>natural language processing</u> and its applications in the
+                  EdTech space.
+                </p>
+              </article>
+            </div>
           </div>
         </section>
 
