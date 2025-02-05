60e2099057a0b80f6d4e69b759087c2a0d3e02c9
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2025-02-05 11:25:24 +0900

    chore: constrain the max width for the hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -40,7 +40,7 @@ export default function HeroSection({
 }) {
   return (
     <section className="py-32">
-      <div className="container">
+      <div className="mx-auto max-w-4xl">
         <div className="grid items-center gap-8 lg:grid-cols-2">
           <div className="flex flex-col items-center text-center lg:items-start lg:text-left">
             <h1 className="my-6 text-pretty text-4xl font-bold lg:text-6xl">
