25d35eaca00c9f627a6e9d43aada1015f35055de
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2025-02-05 15:10:22 +0900

    chore: add padding

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -69,7 +69,7 @@ export default function HeroSection() {
         </div>
       </div>
 
-      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2">
+      <div className="absolute bottom-16 left-1/2 transform -translate-x-1/2">
         <a
           href="#whoami"
           className="group link-outline animate-bounce hidden md:flex items-center flex-col"
