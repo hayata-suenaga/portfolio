a620d0ecba1040b5c10add9bcc5770be39d7fa43
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2025-02-05 18:22:10 +0900

    chore: add padding to the top of the document when scrolling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="scroll-pt-[80px]">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
