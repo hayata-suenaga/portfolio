96283dc0f22077d357f95130cb37a22b08d06f45
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2025-02-05 20:14:23 +0900

    chore: add padding to the footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div className="py-12 bg-muted">
-      <div className="mx-auto max-w-6xl flex flex-col justify-between gap-4 font-medium text-sm lg:flex-row lg:items-center lg:text-left">
+    <div className="py-12 bg-muted px-4">
+      <div className="mx-auto max-w-6xl flex flex-col justify-between gap-4 font-medium text-sm md:flex-row md:items-center md:text-left">
         <p>
           <span className="mr-1 font-bold">hayata.io</span>© All rights
           reserved.
