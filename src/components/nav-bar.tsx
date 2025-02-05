226298801b3f7c05da8642f68ff4a7a070c3fab1
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2025-02-05 20:03:37 +0900

    chore: increase the header width

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -22,7 +22,7 @@ export default function Header() {
   return (
     <header
       className={cn(
-        "fixed inset-x-0 top-4 z-40 mx-auto flex max-w-4xl items-center justify-between rounded-2xl px-8 saturate-100 backdrop-blur-md h-16 font-semibold text-sm text-foreground/60 bg-background/75 border border-border/50"
+        "fixed inset-x-0 top-4 z-40 mx-auto flex max-w-6xl items-center justify-between rounded-2xl px-8 saturate-100 backdrop-blur-md h-16 font-semibold text-sm text-foreground/60 bg-background/75 border border-border/50"
       )}
     >
       <Link href="/" className="flex items-center justify-center gap-1">
