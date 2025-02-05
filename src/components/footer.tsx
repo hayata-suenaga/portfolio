c7f64cbb315afbc56a4603d5c85537337f8303af
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2025-02-05 20:03:22 +0900

    feat: add footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <div className="py-12 bg-muted">
+      <div className="mx-auto max-w-6xl flex flex-col justify-between gap-4 font-medium text-sm lg:flex-row lg:items-center lg:text-left">
+        <p>
+          <span className="mr-1 font-bold">hayata.io</span>© All rights
+          reserved.
+        </p>
+        <p>Made with ❤️ by Hayata Suenaga</p>
+      </div>
+    </div>
+  );
+}
