38f68878efbf8ae37dc833c233b4c07f539bbee0
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2025-02-06 12:33:52 +0900

    feat: add a hook to get the active section id

---
+++ src/lib/useScrollSpy.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+const useScrollspy = (ids: string[], offset: number = 0) => {
+  const [activeId, setActiveId] = useState("");
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveId(entry.target.id);
+          }
+        });
+      },
+      { rootMargin: `${-offset}px 0px 0px 0px`, threshold: 0.2 }
+    );
+
+    ids.forEach((id) => {
+      const element = document.getElementById(id);
+      if (element) {
+        observer.observe(element);
+      }
+    });
+
+    return () => {
+      ids.forEach((id) => {
+        const element = document.getElementById(id);
+        if (element) {
+          observer.unobserve(element);
+        }
+      });
+    };
+  }, [ids, offset]);
+
+  return activeId;
+};
+
+export default useScrollspy;
